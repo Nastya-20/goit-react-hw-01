d45f7853e7658fb345cb5d33cfd8e4778803b766
Your NameNastya-20 <asyataa2016@gmail.com>
2024-08-08 23:31:14 +0300

    add

---
+++ src/components/App/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Profile from '../Profile/Profile';
+//import FriendList from '../FriendList/FriendList';
+//import TransactionHistory from '../TransactionHistory/TransactionHistory';
+import userData from '../../userData.json';
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </div>
+  );
+};
+
+
+
+
+
