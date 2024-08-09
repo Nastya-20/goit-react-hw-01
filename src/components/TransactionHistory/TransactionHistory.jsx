653480a17c3f647f5e0f3d5f078ca45769e98252
Your NameNastya-20 <asyataa2016@gmail.com>
2024-08-09 22:39:24 +0300

    add

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -15,8 +15,8 @@ export default function TransactionHistory({ items }) {
                     {items.map(({ id, type, amount, currency }) => (
                         <tr key={id}>
                             <td className={css.type}>{type}</td>
-                            <td className={css.type}>{amount}</td>
-                            <td className={css.type}>{currency}</td>
+                            <td className={css.amount}>{amount}</td>
+                            <td className={css.currency}>{currency}</td>
                         </tr>
                     ))}
                 </tbody>
