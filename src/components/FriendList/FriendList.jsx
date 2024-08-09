fc140c9dc44a0cd2e8eafe0c6196e5e013790d23
Your NameNastya-20 <asyataa2016@gmail.com>
2024-08-09 23:17:40 +0300

    add

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,19 +1,15 @@
+import FriendListItem from '../FriendListItem/FriendListItem';
 import css from './FriendList.module.css';
 
 export default function FriendList({ friends }) {
     return (
-        <div className={css.container}>
         <ul className={css.friendList}>
             {friends.map(({ id, avatar, name, isOnline }) => (
-                <li key={id} className={css.friendItem}>
-                    <img src={avatar} alt={`${name} avatar`} className={css.avatar} />
-                    <p className={css.nameFriend}>{name}</p>
-                    <span className={isOnline ? css.online : css.offline}>
-                        {isOnline ? 'Online' : 'Offline'}
-                    </span>
+                <li key={id} className={css.friendListItem}>
+                    <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
                 </li>
             ))}
-            </ul>
-       </div>
+        </ul>
     );
 }
+
